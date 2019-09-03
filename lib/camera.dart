d25c200ced69264af6cedcbf5591f6cd43649d4a
Yash Rastogi <yashrastogi11@gmail.com>
2019-09-03 13:10:44 +0530

    Delete unnecessary files

--- lib/camera.dart
+++ lib/camera.dart
@@ -3,7 +3,6 @@ import 'dart:math';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-import 'package:health/chatbot.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
